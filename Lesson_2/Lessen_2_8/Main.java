2c859e94b9acff70e8c24f409f7504bf7b44ba0a
Lelishna-krab <lelishnina@gmail.com>
2025-10-27 12:14:19 +0500

    Initial Commit

--- Lesson_2/Lessen_2_8/Main.java
+++ Lesson_2/Lessen_2_8/Main.java
@@ -1,5 +1,13 @@
 package Lessen_2_8;
 
 public class Main {
+    public static void printStringMultipleTimes(String text, int count) {
+        for (int i = 1; i <= count; i++) {
+            System.out.println(i + ": " + text);
+        }
+    }
 
+    public static void main(String[] args) {
+        printStringMultipleTimes("Привет", 5);
+    }
 }
