a29d1637db155a134164f910f45d15a46b5daeba
Lelishna-krab <lelishnina@gmail.com>
2025-10-27 12:20:38 +0500

    Initial Commit

---
+++ Lesson_2/Lessen_2_10/Main.java
@@ -0,0 +1,28 @@
+package Lessen_2_10;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        System.out.print("Исходный массив: ");
+        printArray(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+        }
+
+        System.out.print("Измененный массив: ");
+        printArray(array);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
